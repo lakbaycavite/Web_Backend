8a2cdc51f4de545db583c0cce313f86d00ad15b9
Lance Ballicud <lmuballicud@gmail.com>
2025-04-09 22:09:45 +0800

    modify background cron

--- utils/eventTimeChecker.js
+++ utils/eventTimeChecker.js
@@ -1,7 +1,7 @@
 const cron = require('node-cron');
 const Event = require('../models/eventModel');
 
-cron.schedule('0 * * * *', async () => {
+cron.schedule('*/1 * * * *', async () => {
     console.log('Checking for expired events...');
     const now = new Date();
 
